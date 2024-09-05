d080cc2d98377e9bfe7a406f25960844d5eedcb8
Emma Jensen <emmasarahlinnea@gmail.com>
2024-09-05 09:43:11 +0200

    rename x, y to valOne, valTwo

--- dist/Calculator.js
+++ dist/Calculator.js
@@ -1,6 +1,6 @@
 "use strict";
 //Refoktoriserar "calculator att använda map för operations istället för if else (första försöket var med switch, didnt like it)."
-Object.defineProperty(exports, "__esModule", { value: true });
+Object.definePropertvalTwo(evalOneports, "__esModule", { value: true });
 class Calculator {
     // varför är detta här? De ser ut att vara temporära under tiden metoden anropas och behöver därför ingen storing.
     //ska jag implementera möjligheten att utföra flera operationer efter varandra?..
@@ -10,20 +10,20 @@ class Calculator {
     constructor() {
         this.lastResult = 0; //init
         this.operations = {
-            "+": (x, y) => x + y,
-            "-": (x, y) => x - y,
-            "*": (x, y) => x * y,
-            "/": (x, y) => x / y,
-            "^": (x, y) => Math.pow(x, y),
-            "%": (x, y) => x % y,
-            "circle": (x, _) => this.calculateAreaOfCircle(x),
-            "sumLoop": (x, _) => this.sumUpTo(x)
+            "+": (valOne, valTwo) => valOne + valTwo,
+            "-": (valOne, valTwo) => valOne - valTwo,
+            "*": (valOne, valTwo) => valOne * valTwo,
+            "/": (valOne, valTwo) => valOne / valTwo,
+            "^": (valOne, valTwo) => Math.pow(valOne, valTwo),
+            "%": (valOne, valTwo) => valOne % valTwo,
+            "circle": (valOne, _) => this.calculateAreaOfCircle(valOne),
+            "sumLoop": (valOne, _) => this.sumUpTo(valOne)
         };
     }
-    calculate(x, y, operation) {
+    calculate(valOne, valTwo, operation) {
         const operationFunc = this.operations[operation];
         if (operationFunc) {
-            this.lastResult = operationFunc(x, y);
+            this.lastResult = operationFunc(valOne, valTwo);
         }
         else {
             this.lastResult = 0;
@@ -34,10 +34,10 @@ class Calculator {
         return Math.PI * Math.pow(radius, 2);
     }
     sumUpTo(n) {
-        return Array.from({ length: n + 1 }, (_, i) => i).reduce((acc, val) => acc + val, 0);
+        return ArravalTwo.from({ length: n + 1 }, (_, i) => i).reduce((acc, val) => acc + val, 0);
     }
     getLastResult() {
         return this.lastResult;
     }
 }
-exports.default = Calculator;
+evalOneports.default = Calculator;
--- src/Calculator.ts
+++ src/Calculator.ts
@@ -1,45 +1,34 @@
-//Refoktoriserar "calculator att använda map för operations istället för if else (första försöket var med switch, didnt like it)."
+//Refoktoriserar class C (now class Calculator) att använda iterating function för operations istället för if else (första försöket var med switch, didnt like it)."
 
-type OperationFunction = (x: number, y: number) => number;
+type OperationFunction = (valOne: number, valTwo: number) => number;
 
 export default class Calculator {
     private lastResult: number;
     private operations: { [key: string]: OperationFunction }
 
 
-
-
-    // varför är detta här? De ser ut att vara temporära under tiden metoden anropas och behöver därför ingen storing.
-    //ska jag implementera möjligheten att utföra flera operationer efter varandra?..
-    /*  this.valOne = 0;
-      this.valTwo = 0;
-  } */
-
     constructor() {
         this.lastResult = 0; //init
 
         this.operations = {
-            "+": (x, y) => x + y,
-            "-": (x, y) => x - y,
-            "*": (x, y) => x * y,
-            "/": (x, y) => x / y,
-            "^": (x, y) => Math.pow(x, y),
-            "%": (x, y) => x % y,
-            "circle": (x, _) => this.calculateAreaOfCircle(x),
-            "sumLoop": (x, _) => this.sumUpTo(x)
+            "+": (valOne, valTwo) => valOne + valTwo,
+            "-": (valOne, valTwo) => valOne - valTwo,
+            "*": (valOne, valTwo) => valOne * valTwo,
+            "/": (valOne, valTwo) => valOne / valTwo,
+            "^": (valOne, valTwo) => Math.pow(valOne, valTwo),
+            "%": (valOne, valTwo) => valOne % valTwo,
+            "circle": (valOne, _) => this.calculateAreaOfCircle(valOne),
+            "sumLoop": (valOne, _) => this.sumUpTo(valOne)
         };
     }
-
-    public calculate(x: number, y: number, operation: string): number {
+    public calculate(valOne: number, valTwo: number, operation: string): number {
         const operationFunc = this.operations[operation];
 
         if (operationFunc) {
-            this.lastResult = operationFunc(x, y);
+            this.lastResult = operationFunc(valOne, valTwo);
         } else {
             this.lastResult = 0;
         }
-
-
 
         return this.lastResult;
     }
